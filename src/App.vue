25b682d3acffe8cc6f54149e4f064aa1b256db2a
moritz <sergeial1972@gmail.com>
2026-03-01 23:33:19 +0300

    grid ready

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<body>
+<div class="app">
+  <div class="app-header"><header><h1>Welcome</h1></header> </div>
+  <div class="main">Main</div>
+  <div class="side">Side</div>
+</div>
+</body>
+</template>
+
+<script>
+</script>
+
+<style>
+.app {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;     /* 3 равные колонки для main:side 2:1 */
+  grid-template-rows: repeat(5, 1fr);     /* 5 строк высотой viewport */
+  height: 100vh;
+  gap: 10px;
+  padding: 20px;
+}
+
+.app-header { grid-area: 1 / 1 / 2 / 4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.main      { grid-area: 2 / 1 / 6 / 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.side      { grid-area: 2 / 3 / 6 / 4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+
+}
+
+</style>
